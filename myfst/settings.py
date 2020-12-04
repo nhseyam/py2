b32820307aeb897adea40dbb7e2b983517f0792f
Nahid Hasan Seyam <75113632+nhseyam@users.noreply.github.com>
2020-12-04 23:51:18 +0600

    Update settings.py

--- myfst/settings.py
+++ myfst/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'y3b#=hffv7csjy@g65)$77(7slkno!%ksgfof^a9ml8-yu5=t*'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
